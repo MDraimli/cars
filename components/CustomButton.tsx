7ac6bd2ca676eeb30efba0325f7e621ac0375e88
Mohammed Draimli <m_fares@live.com>
2024-09-10 18:29:54 +0300

    Adding Public folder

---
+++ components/CustomButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Image from "next/image";
+
+export default function CustomButton() {
+  return (
+    <button
+        disabled={false}
+        type= {"button"}
+        className={`custom-btn`}
+        onClick={() => {}}
+    >
+
+    </button>
+  )
+}
